7b57d16e956be236eeb18e0927bcb11f32c39cd0
KuroEinzbern <Alejandro.Damian.Bastida@hotmail.com>
2026-05-07 16:43:39 -0300

    notebook modelo

---
+++ src/default_risk/script/auxiliar_eda_function.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from scipy.stats import trim_mean       
+from IPython.display import display
+import numpy as np
+import default_risk.config as cfg
+import json
+
+def recreate_and_sort_the_serie_given_ids(ids : pd.Series, df : pd.DataFrame,column_time_name) -> pd.DataFrame :
+    recreated_series= df[df["SK_ID_PREV"].isin(ids)]
+    return recreated_series.sort_values(["SK_ID_PREV",column_time_name])
+     
+
+def recreate_and_sort_series_given_rows(rows : pd.DataFrame, df : pd.DataFrame, column_time_name : str) -> pd.DataFrame :
+    id_rows= rows["SK_ID_PREV"].unique()
+    return recreate_and_sort_the_serie_given_ids(id_rows, df,column_time_name)
