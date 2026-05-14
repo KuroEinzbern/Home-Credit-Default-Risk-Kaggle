52e772d021c16a56fc740bcd38fbf102d0838333
Ignacio Pugliese <ignacio@namastay.io>
2026-05-15 01:46:39 +0200

    clean pos cash balance

--- src/default_risk/make_dataset.py
+++ src/default_risk/make_dataset.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from types import SimpleNamespace
 from default_risk.config import CLEANS_DIR, RAW_DATA_DIR, SPLITS_DIR
-from default_risk.datasets.clean import clean_credit_card_balance, clean_installments_payments
+from default_risk.datasets.clean import clean_credit_card_balance, clean_installments_payments, clean_pos_cash_balance
 from default_risk.datasets.extract import split_dataset
 
 def generate_cleaner_paths(table_name: str, split: str):
     p_in = SPLITS_DIR / f'{table_name}.{split}.parquet'
-    p_out = CLEANS_DIR / f'1.0-{table_name}.{split}.parquet'
+    p_out = CLEANS_DIR / f'{table_name}.{split}-cleaned.parquet'
     return p_in, p_out
 
 def main():
@@ -24,6 +24,9 @@ def main():
 
         installment_payments_in, installment_payments_out = generate_cleaner_paths('installments_payments', split)
         clean_installments_payments(installment_payments_in, installment_payments_out)
+
+        pos_cash_in, pos_cash_out = generate_cleaner_paths('POS_CASH_balance', split)
+        clean_pos_cash_balance(pos_cash_in, pos_cash_out)
 
 
 
